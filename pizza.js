d1b876a365d7f8fcfb034c96e5ed8d4ccf333225
moryw <matt@skitskat.xyz>
2018-03-16 14:33:57 -0400

    Add orderCount, Subtotal, tax, and Total.

--- pizza.js
+++ pizza.js
@@ -1,4 +1,23 @@
+let orderCount = 0;
+let tax = 1.14;
 const takeOrder = (topping, crustType) => {
-  console.log(`Order: ${crustType} pizza topped with ${topping}`);
+  orderCount ++;
+  console.log(`Order: ${crustType} crust pizza topped with ${topping}`);
 };
-takeOrder('cheese');
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+const getTotal = (tax, subTotal) => {
+  return tax * subTotal;
+};
+
+//getTotal = getTotal.toFixed(2);
+
+takeOrder('cheese','thin');
+takeOrder('peperoni','regular');
+takeOrder('bacon double cheese','thick');
+console.log(`Subtotal: $${getSubTotal(orderCount)}0`);
+console.log('     Tax:    14%');
+console.log(`   Total: $${getTotal(tax, getSubTotal(orderCount))}`);
