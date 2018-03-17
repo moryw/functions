369874f36e8121018e59f5874750ed9832a0375f
moryw <matt@skitskat.xyz>
2018-03-17 17:34:50 -0400

    Add getTax and getTotal functions and log the results.

--- pizza.js
+++ pizza.js
@@ -9,8 +9,12 @@ const getSubTotal = (itemCount) => {
   return itemCount * 7.5;
 };
 
-const getTotal = (tax, subTotal) => {
-  return tax * subTotal;
+const getTax = () => {
+  return getSubTotal(orderCount) * 0.06;
+};
+
+const getTotal = () => {
+  return getSubTotal(orderCount) + getTax();
 };
 
 //getTotal = getTotal.toFixed(2);
@@ -19,5 +23,5 @@ takeOrder('cheese','thin');
 takeOrder('peperoni','regular');
 takeOrder('bacon double cheese','thick');
 console.log(`Subtotal: $${getSubTotal(orderCount)}0`);
-console.log('     Tax:    14%');
-console.log(`   Total: $${getTotal(tax, getSubTotal(orderCount))}`);
+console.log('     Tax:    6%');
+console.log(`   Total: $${getTotal()}`);
