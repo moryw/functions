3dcf4406b6d1a6d41cbbb0f87760a71c0257ed37
moryw <matt@skitskat.xyz>
2018-03-21 10:33:55 -0400

    Add function expression syntax and arrow function syntax variations.

--- greaterThan.js
+++ greaterThan.js
@@ -1,9 +1,31 @@
-function isGreaterThan(numberOne, numberTwo) {
+// This is a function DECLARATION, it does not need to end in a semi semicolon.
+
+// function isGreaterThan(numberOne, numberTwo) {
+//   if (numberOne > numberTwo) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+//This is a function EXPRESSION, it needs to end in a semicolon because it is inside a variablbe.
+
+// const isGreaterThan = function (numberOne, numberTwo) {
+//   if (numberOne > numberTwo) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+//This is a function EXPRESSION using arrow function syntax.
+
+const isGreaterThan = (numberOne, numberTwo) => {
   if (numberOne > numberTwo) {
     return true;
   } else {
     return false;
   }
-}
+};
 
-console.log(isGreaterThan(2,1));;
+console.log(isGreaterThan((3*10),(65/2)));;
